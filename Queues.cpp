fd48d4ff6932c70326bfe1fb19a8024f08a4c289
muhammadichsanulkahfi <muhammadichsanulkahfi@gmail.com>
2025-06-12 15:41:18 +0700

    Jika FRONT_position > REAR_position, iterasi dari FRONT hingga akhir array

--- Queues.cpp
+++ Queues.cpp
@@ -96,7 +96,7 @@ public:
             }
             cout << endl;
         }
-        else
+        else 
         {
             // Jika FRONT_position > REAR_position, iterasi dari FRONT hingga akhir array
             while (FRONT_position <= max - 1)
